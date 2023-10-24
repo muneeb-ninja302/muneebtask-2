60426e9542f286d1bd182c00e93c5f27f1e20fa2
muneeb-ninja302 <143833760+muneeb-ninja302@users.noreply.github.com>
2023-10-24 20:03:19 +0500

    java script 1st task 2nd week of internship

---
+++ script.js
@@ -0,0 +1,9 @@
+let InformationBtn= document.querySelector ('button');
+InformationBtn.addEventListener ('click', inputMsg)
+function inputMsg(){
+
+let name = prompt('enter name of student');
+InformationBtn.textContent= 'Roll NO. 26'
+
+
+}
